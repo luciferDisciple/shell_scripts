c14ac6074f32f463aa0fafab667f3e721e736cfd
luciferDisciple <waizer12@gmail.com>
2023-02-13 16:19:07 +0100

    help text, usage text, and cli option parsing

--- gitignore.sh
+++ gitignore.sh
@@ -1,19 +1,61 @@
 #!/bin/bash
 
-language="$1"
+PROG=gitignore
 
-die() {
-    local message="$@"
-    echo $message >&2
-    exit 1
+usage() {
+	echo "usage: $PROG [-h|--help] LANG" >&2
 }
 
-if [[ $# -ne 1 ]]; then
-	die Wrong number of arguments. Required: 1
+print_help() {
+	usage
+	cat >&2 <<-END
+	Put a .gitignore file in the current working directory obtained from
+	https://github.com/github/gitignore for a specific language or
+	runtime environment.
+	
+	positional arguments:
+	  LANG        language or framework that you want to get a .gitignore for
+	
+	optional arguments:
+	  -h, --help   show this help message and exit
+	END
+}
+
+error() {
+	local message="$@"
+	echo "$PROG: error: $message" >&2
+	exit 1
+}
+
+usage_error() {
+	local message="$@"
+	usage
+	error "$message"
+}
+
+while :; do
+	case "$1" in
+		-h|--help)
+			print_help
+			exit
+			;;
+		-*)
+			usage_error unrecognized option: $1
+			;;
+		*)
+			break
+	esac
+	shift
+done
+
+language="$1"
+
+if [[ -z $language ]]; then
+	usage_error the following arguments are required: LANG
 fi
 
 if [[ -f .gitignore ]]; then
-	die File already .gitignore present in the current directory. Aborting.
+	error 'File .gitignore already present in the current directory. Aborting.'
 fi
 
 # massage "language" argument
